79e3e5e77d2d04267fcdfb8cf33c30eb958e5426
Doston-1097 <dostonbekbozorboyev7@gmail.com>
2022-03-09 19:58:00 +0500

    adada

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import {Container} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 
 function Footer(props) {
     return (
-        <Paper sx={{backgroundColor:"#C4C4C4",mt:"100px",borderRadius:"0"}}>
+        <Paper sx={{backgroundColor:"#C4C4C4 !important",mt:"100px",borderRadius:"0"}}>
             <Container sx={{py:"20px",display:"flex",justifyContent: 'space-between',alignItems:"center"}}>
-                <img src="./images/Logo.svg" alt=""/>
-                <img src="./images/Algorismic.svg" alt="" onClick={()=>window.open(`https://www.algorismic.uz/`)}/>
+
+
+                <Grid container spacing={2} sx={{display:"flex",justifyContent:"space-between",alignItems:'center'}}>
+                    <Grid item>
+                        <img src="./images/Logo.svg" alt=""/>
+                    </Grid>
+                    <Grid item>
+                        <img src="./images/Algorismic.svg" alt="" onClick={()=>window.open(`https://www.algorismic.uz/`)}/>
+                    </Grid>
+                </Grid>
             </Container>
 
         </Paper>
